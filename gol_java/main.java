918af613817e3effc844e8dcfb85329a56847595
LeDanix <74117305+LeDanix@users.noreply.github.com>
2023-07-14 23:05:12 +0200

    wait millis

--- gol_java/main.java
+++ gol_java/main.java
@@ -4,6 +4,9 @@
 // Powered by JDK 20
 ////////////////////////////////////////////
 import src.graphics_;
+
+import javax.swing.SwingUtilities;
+
 import src.Matrix;
 
 
@@ -11,25 +14,25 @@ class GameOfLife{
     private static byte ROWS = 100;
     private static byte COLUMNS = 50;
     private static byte MAX_ITERATIONS = 100;
-    private static boolean ACTIVE = true;
+
+    private static void wait(int millis){
+        try {
+            Thread.sleep(millis);
+        } catch (InterruptedException e) {
+            Thread.currentThread().interrupt();
+        }
+    }
 
     public static void main(String[] args){
         byte iter = 0;
         Matrix matrix = new Matrix(ROWS, COLUMNS);
         graphics_ ghaphics = new graphics_(matrix);
-        ghaphics.setVisible(ACTIVE);
+        ghaphics.setVisible(true);
         while (iter++ < MAX_ITERATIONS){
             matrix.update();
             ghaphics = new graphics_(matrix);
+            ghaphics.setVisible(true);
+            wait(50);
         }
-
-
-
-
-
-
-
-
-
     }
 }
